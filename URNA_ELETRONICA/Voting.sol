86b3269a0039be56a8b587d724dd28ba8304b236
orcus69 <josusantos2@gmail.com>
2022-03-06 10:12:00 -0300

    using struct type

--- URNA_ELETRONICA/Voting.sol
+++ URNA_ELETRONICA/Voting.sol
@@ -1,40 +1,41 @@
 //SPDX-License-Identifier: GPL-2.0-or-later
-
 //especificar a versão do solidity
-pragma solidity >= 0.8.1;
+pragma solidity = 0.8.1;
 
 contract Voting{
-    //public variables are visible for anyone out of the contract
-    address public admin;
-    string public pauta;
+ //public variables are visible for anyone out of the contract
+    
+    struct Pauta {
+        string pauta;
+        bool voted;
+    }
 
-    //the options are enum like [ 0, 1, 2, 3, 4 ]
+    address admin;
+
+    mapping (address => Pauta) pauta;
+
     enum Option{ Yes, No, Null, Abstention }
-
     //store an array of adress fo each one option
     mapping( Option => address[] ) vote;
-
-    //map with users tha already voted
-    mapping( address => bool ) user;
 
     //create the contrate with one pauta to be voting
     constructor( string memory _pauta ){
         admin = msg.sender;
-        pauta = _pauta;
+        pauta[msg.sender].pauta = _pauta;
     }
 
     //Vote Function
     function votate ( Option _option ) public {
         //verify if user is in user object,
         //if its true show the message user alredy voted
-        require( !user[msg.sender], "User alredy voted!" );
+        require( !pauta[msg.sender].voted, "User alredy voted!" );
 
         //insert into vote object the address of who vote
         vote[_option].push(msg.sender);
 
         //add user addres into object user as true,
         //means that he fish his votation
-        user[msg.sender] = true;
+        pauta[msg.sender].voted = true;
     }
 
     //View Result of Votation
@@ -42,5 +43,14 @@ contract Voting{
         return( vote[_option] );
     }
 
-}
+    //return admin
+    function getAdmin() public view returns(address){
+        return(admin);
+    }
+    
+    //return agenda
+    function getAgenda() public view returns(string memory){
+        return(pauta[admin].pauta);
+    }
 
+}
